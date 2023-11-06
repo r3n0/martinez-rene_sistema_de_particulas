771be610593129c2834454fced37d5aed26acffa
René Martínez <hola@r3n0.com>
2023-11-06 16:19:56 -0500

    updates

--- Particula.js
+++ Particula.js
@@ -3,15 +3,16 @@ class Particula {
 		this.tamanoInicial = random(10, 50);
 		this.tamano = this.tamanoInicial;
 		this.estaMuerta = false;
-		this.tiempoDeVidaInicial = Math.ceil(random(10, 80));
+		this.tiempoDeVidaInicial = Math.ceil(random(10, 300));
 		this.tiempoDeVida = this.tiempoDeVidaInicial;
 
 		this.pos = createVector(mouseX, mouseY);
 
-		this.vel = createVector(0, random(3, 5));
+		this.vel = createVector(0, random(1, 3));
 		this.vel.rotate(random(360));
 
-		this.g = createVector(0, 0.1);
+		this.g = createVector(0.1, -0.05);
+		this.color = color(random(100, 200), 255, 0, 50);
 	}
 	// Método ==> method update
 	actualizar() {
@@ -22,8 +23,8 @@ class Particula {
 			this.tiempoDeVida,
 			0,
 			this.tiempoDeVidaInicial,
-			0,
-			this.tamanoInicial
+			this.tamanoInicial,
+			0
 		);
 
 		this.tiempoDeVida -= 1;
@@ -36,6 +37,8 @@ class Particula {
 	}
 	// Método ==> method display
 	mostrar() {
+		noStroke();
+		fill(this.color);
 		circle(this.pos.x, this.pos.y, this.tamano);
 	}
 }
--- sketch.js
+++ sketch.js
@@ -6,10 +6,14 @@ function setup() {
 }
 
 function draw() {
-	background(255, 50);
+	background(0, 50);
 
-	let p = new Particula();
-	sistema.push(p);
+	let p1 = new Particula();
+	sistema.push(p1);
+	let p2 = new Particula();
+	sistema.push(p2);
+	let p3 = new Particula();
+	sistema.push(p3);
 
 	for (let i = 0; i < sistema.length; i++) {
 		if (sistema[i].estaMuerta) {
